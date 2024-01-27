b3964bd159c81169ce771825efa507dfa6d45c56
ovi <sbswag9@gmail.com>
2024-01-28 02:25:33 +0600

    added footer without contact us modal

--- verge/app/page.tsx
+++ verge/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Footer from "./components/Footer";
 
 export default function Page() {
   return (
     <>
     <Header/>
     <Home/>
+    <Footer/>
     </>
   );
 }
